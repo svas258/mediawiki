612c1bc74c69481d32b9e600ac15c292ebce8d53
svas258 <91326469+svas258@users.noreply.github.com>
2022-11-21 10:10:20 +0530

    Update variables.tf

--- Azure/variables.tf
+++ Azure/variables.tf
@@ -30,3 +30,20 @@ variable "appId" {
 variable "password" {
   description = "Azure Kubernetes Service Cluster password"
 }
+
+variable "client_id" {
+  type        = string
+  description = "client_id"
+}
+variable "client_secret" {
+  type        = string
+  description = "client_secret"
+}
+variable "tenant_id" {
+  type        = string
+  description = "tenant_id"
+}
+variable "subscription_id" {
+  type        = string
+  description = "subscription_id"
+}
